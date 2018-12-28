6ded8ba3a37b72d4014de5e62aab51192c3d6bbc
sonnyhermo <sonnyboyhermo@gmail.com>
2018-12-28 08:44:06 +0800

    test repo
    test

---
+++ app/Http/Controllers/TestControllers/MyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\TestControllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MyController extends Controller
+{
+    //
+}
